35c6deb02dacf22264da51e4263aa570bc8979fe
akoross <74363342+akoross@users.noreply.github.com>
2020-11-13 07:53:34 +0200

    Add test-project

---
+++ slick/slider-setings.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      //   autoplay: true,
+      //   autoplaySpeed: 1000,
+    });
+  });
